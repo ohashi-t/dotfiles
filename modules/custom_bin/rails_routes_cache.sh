f253386984b40b358dcb7cd7b8c2dbed229d219b
Ohashi <66351991+ohashi-t@users.noreply.github.com>
2022-02-28 13:09:00 +0900

    sed: gnu-sedに変更(gsed)

--- modules/custom_bin/rails_routes_cache.sh
+++ modules/custom_bin/rails_routes_cache.sh
@@ -1,6 +1,6 @@
-set -eu
+set -eux
 
 test -n "${1}" || exit -1
 
 test ! -d "${1}/vendor" || mkdir -p "${1}/vendor"
-echo $(GEMFILE="${1}/Gemfile" bundle exec rails routes) | sed -E 's/ (GET|POST|PUT|PATCH|DELETE)/\n\1/g' > ${1}/vendor/tmp_rails_routes.cache
+echo $(GEMFILE="${1}/Gemfile" bundle exec rails routes) | gsed -E 's/ (GET|POST|PUT|PATCH|DELETE)/\n\1/g' > ${1}/vendor/tmp_rails_routes.cache
